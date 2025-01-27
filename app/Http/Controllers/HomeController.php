c3d068509054333e3fc937b18a6b18db37772400
Sngt25 <jeddycertifico25@gmail.com>
2025-01-27 15:14:24 +0800

    feat(app): add EventPost and NewsUpdate resources to HomeController index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CscDirectoryResource;
+use App\Http\Resources\EventPostResource;
+use App\Http\Resources\NewsUpdateResource;
 use App\Http\Resources\UscAdviserResource;
 use App\Http\Resources\UscResource;
 use App\Models\CscDirectory;
+use App\Models\EventPost;
+use App\Models\NewsUpdate;
 use App\Models\UscAdviser;
 use App\Models\UscOfficial;
 use Illuminate\Http\Request;
@@ -17,11 +21,15 @@ class HomeController extends Controller
         $uscs = UscOfficial::all();
         $usc_advisers = UscAdviser::all();
         $cscs = CscDirectory::all();
+        $events = EventPost::latest()->take(6)->get();
+        $news_updates = NewsUpdate::latest()->take(5)->get();
 
         return response()->json([
             'uscs' => UscResource::collection($uscs),
             'usc_advisers' => UscAdviserResource::collection($usc_advisers),
             'cscs' => CscDirectoryResource::collection($cscs),
+            'events' => EventPostResource::collection($events),
+            'news_updates' => NewsUpdateResource::collection($news_updates),
         ]);
     }
 }
