3a683512ee56c192802ad600054086f2e741db5a
Sngt25 <jeddycertifico25@gmail.com>
2025-03-07 02:59:09 +0800

    chore: update routes depending on roles

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::apiResource('news-updates', NewsUpdateController::class)->only(['index'])
 Route::apiResource('reports', ReportController::class)->only(['index']);
 Route::get('faqs', [FaqController::class, 'index']);
 Route::get('judicials', [JudicialController::class, 'index']);
+Route::post('documents/show', [DocumentController::class, 'show']);
 
 Route::apiResource('event-posts', EventPostController::class)->except(['show']);
 Route::apiResource('news-updates', NewsUpdateController::class)->except(['show']);
@@ -69,44 +70,25 @@ Route::middleware(['auth', 'role:pio'])->group(function () {
     });
 });
 
+Route::middleware(['auth', 'role:ivc'])->group(function () {
+    Route::get('documents', [DocumentController::class, 'index']);
+    Route::post('documents', [DocumentController::class, 'store']);
+    Route::put('documents/{id}/status', [DocumentController::class, 'update']);
+    Route::delete('documents/{id}', [DocumentController::class, 'destroy']);
+});
+
+Route::middleware(['auth', 'role:auditor'])->group(function () {
+    Route::prefix('evaluations')->group(function () {
+        Route::get('/', [EvaluationController::class, 'index']); // Get all evaluations
+        Route::put('/{evaluation}', [EvaluationController::class, 'update']); // Update an evaluation
+        Route::post('/search', [EvaluationController::class, 'search']); // Search evaluations by event title
+    });
+});
+
 Route::post('/generate-report', [ReportController::class, 'generateReport']);
-
-// List all documents
-Route::get('documents', [DocumentController::class, 'index']);
-
-// Create a new document
-Route::post('documents', [DocumentController::class, 'store']);
-
-// Show a specific document
-Route::post('documents/show', [DocumentController::class, 'show']);
-
-// Update the status of a document
-Route::put('documents/{id}/status', [DocumentController::class, 'update']);
-
-// Delete a document
-Route::delete('documents/{id}', [DocumentController::class, 'destroy']);
 
 Route::post('activity-log', [ActivityLogController::class, 'store']);
 // Activity Log APIs
 Route::get('activity-log/public-relations', [ActivityLogController::class, 'publicRelations']);
 Route::get('activity-log/audit-reports', [ActivityLogController::class, 'auditReports']);
 Route::get('activity-log/file-tracking', [ActivityLogController::class, 'fileTracking']);
-
-// Auditor routes
-// Route::middleware(['auth', 'role:auditor'])->group(function () {
-//     Route::prefix('evaluations')->group(function () {
-//         Route::get('/', [EvaluationController::class, 'index']); // Get all evaluations
-//         Route::post('/', [EvaluationController::class, 'store']); // Create a new evaluation
-//         Route::get('/{evaluation}', [EvaluationController::class, 'show']); // Get a specific evaluation
-//         Route::put('/{evaluation}', [EvaluationController::class, 'update']); // Update an evaluation
-//         Route::delete('/{evaluation}', [EvaluationController::class, 'destroy']); // Delete an evaluation
-//         Route::post('/search', [EvaluationController::class, 'search']); // Search evaluations by event title
-//     });
-// });
-
-//No auth for testing only
-Route::prefix('evaluations')->group(function () {
-    Route::get('/', [EvaluationController::class, 'index']); // Get all evaluations
-    Route::put('/{evaluation}', [EvaluationController::class, 'update']); // Update an evaluation
-    Route::post('/search', [EvaluationController::class, 'search']); // Search evaluations by event title
-});
