91419861bfcfc35685e596ee7b2f8823a24906f8
Sngt25 <jeddycertifico25@gmail.com>
2025-03-10 21:20:01 +0800

    chore: default to lowercase glc members

--- app/Models/GlcVotingNonVoting.php
+++ app/Models/GlcVotingNonVoting.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class GlcVotingNonVoting extends Model
 {
     protected $guarded = ['id'];
+
+    public function getNameAttribute($value)
+    {
+        return ucwords(strtolower($value));
+    }
 }
