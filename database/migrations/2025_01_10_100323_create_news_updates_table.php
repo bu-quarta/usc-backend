eaf1ffba53e2619f76c20b586886cf2d87f8e443
AlvinNario <alvinsalle.nario@bicol-u.edu.ph>
2025-03-12 09:14:04 +0800

    feat: add fields for posted by and layout by information in NewsUpdate model, controller, and migration

--- database/migrations/2025_01_10_100323_create_news_updates_table.php
+++ database/migrations/2025_01_10_100323_create_news_updates_table.php
@@ -18,6 +18,10 @@ class CreateNewsUpdatesTable extends Migration
             $table->string('title'); // title of the news/update
             $table->string('slug')->unique(); // slug for the news-update
             $table->text('description'); // content of the news/update
+            $table->string('posted_by_name'); // name of the person who posted
+            $table->string('posted_by_position'); // position of the person who posted
+            $table->string('layout_by_name'); // name of the person responsible for layout
+            $table->string('layout_by_position'); // position of the person responsible for layout
             $table->timestamp('publish_date')->useCurrent(); // publish date (defaults to current timestamp)
             $table->string('image_path')->nullable(); // path to the image
             $table->enum('status', ['published', 'draft'])->default('draft'); // status of the news/update
